127f5a71ac5998788e2dee21cec099ffe5ebdf33
Connie Chen <spiritgreen@gmail.com>
2011-10-30 19:29:10 -0700

    enable admin

--- scores/models.py
+++ scores/models.py
@@ -9,5 +9,22 @@ class Player(models.Model):
   date_joined = models.DateTimeField('date joined')
   email = models.CharField(max_length=20)
 
-#class PlayerScore(models.Model):
-#  player = models
+class PlayerScore(models.Model):
+  player = models.ForeignKey(Player)
+  game = models.ForeignKey(Game)
+  fields = models.IntegerField()
+  pastures = models.IntegerField()
+  grain = models.IntegerField()
+  vegetables = models.IntegerField()
+  sheep = models.IntegerField()
+  wild_boar = models.IntegerField()
+  cattle = models.IntegerField()
+  unused_spaces = models.IntegerField()
+  fenced_stables = models.IntegerField()
+  clay_rooms = models.IntegerField()
+  stone_rooms = models.IntegerField()
+  family = models.IntegerField()
+  card_points = models.IntegerField()
+  bonus_points = models.IntegerField()
+  
+  
