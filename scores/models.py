b61adf1aa0ab0b0cd2fe48df5d3704482e71bf72
Connie Chen <spiritgreen@gmail.com>
2011-11-06 15:29:21 -0800

    oops

--- scores/models.py
+++ scores/models.py
@@ -1,13 +1,35 @@
 from django.db import models
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Game(models.Model):
   deck = models.CharField(max_length=10)
   date = models.DateTimeField('date')
 
+  def number_of_players(self):
+    return self.playerscore_set.count()
+
+  def game_winner(self):
+	players = self.player_set.all()
+	print "hello"
+	scores = [player.total_score() for player in players]
+	winner = max(scores)
+	return winner.name
+
+
+
 class Player(models.Model):
   name = models.CharField(max_length=20)
   date_joined = models.DateTimeField('date joined')
   email = models.CharField(max_length=20)
+
+  def __unicode__(self):
+	print "whee"
+	return self.name
+
+  def number_of_games_played(self):
+	return self.playerscore_set.count()
 
 class PlayerScore(models.Model):
   player = models.ForeignKey(Player)
@@ -26,5 +48,15 @@ class PlayerScore(models.Model):
   family = models.IntegerField()
   card_points = models.IntegerField()
   bonus_points = models.IntegerField()
-  
+
+  def player_name(self):
+	return self.player.name
+
+  def game_date(self):
+	return self.game.date
+
+  def total_score(self):
+	return self.fields+self.pastures+self.grain+self.vegetables+self.sheep+self.wild_boar \
+			+self.cattle+self.unused_spaces+self.fenced_stables+self.clay_rooms+self.stone_rooms \
+			+self.family+self.card_points+self.bonus_points
   
